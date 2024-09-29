23a0f63ad2bbac9c49dc0e34d2f0befbfd14bad5
Riddhi Airan <datascience.riddhi@gmail.com>
2024-09-29 16:40:31 +0530

    updated

--- Day-02/Tip_Calculator.py
+++ Day-02/Tip_Calculator.py
@@ -2,10 +2,14 @@ import streamlit as st
 
 st.title("Tip Calculator")
 
-Bill = st.number_input("What was the total Bill amount? ")
-Tip = st.number_input("How much tip would you like to give? ", min_value=0.0, format="%.2f")
-Split = st.number_input("How many people to split the bill? (at least 1)", min_value=1)
+# Get inputs
+Bill = st.number_input("What was the total Bill amount?", value=0.0, format="%.2f")
+Tip = st.number_input("How much tip would you like to give?", min_value=0.0, value=0.0, format="%.2f")
+Split = st.number_input("How many people to split the bill? (at least 1)", min_value=1, value=1)
 
-total = (Bill + Tip) / Split
-
-st.write(f"Each person should pay: {total:.2f}")
+# Calculate total payment per person
+if Split > 0:  # Although it's redundant here due to the input restrictions
+    total = (Bill + Tip) / Split
+    st.write(f"Each person should pay: {total:.2f}")
+else:
+    st.write("Please enter a valid number of people.")
