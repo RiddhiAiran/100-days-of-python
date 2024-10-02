9a0ec330313006ca464a7544583bed7b7fac0cfc
Riddhi Airan <datascience.riddhi@gmail.com>
2024-10-02 19:49:24 +0530

    update rockpaper

--- Day-04/rock,paper,scissors.py
+++ Day-04/rock,paper,scissors.py
@@ -1,14 +1,65 @@
 import random
+import streamlit as st
 
-rock = ""
+# ASCII Art for Rock, Paper, Scissors
+rock = '''
 
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
 
-paper = ""
+paper = '''
 
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
 
-scissors = ""
+scissors = '''
 
-user_input = input("What do you choose? Type 0 for Rock, 1 for paper and 2 for scissors\n")
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
 
-computer = random.randint(0,2)
-print(f"Computer chose : {computer}")
+items = [rock, paper, scissors]
+
+# Streamlit UI
+st.title("Rock, Paper, Scissors Game")
+
+# User choice with a dropdown menu
+user_choice = st.selectbox("What do you choose?", ["Rock", "Paper", "Scissors"])
+
+# Convert user choice to corresponding index
+user = ["Rock", "Paper", "Scissors"].index(user_choice)
+
+if st.button("Play!"):
+    # Display user's choice
+    st.write(f"You chose: {user_choice}")
+    st.code(items[user])  # Display ASCII art for user's choice
+
+    # Computer's random choice
+    computer = random.randint(0, 2)
+    computer_choice = ["Rock", "Paper", "Scissors"][computer]
+
+    # Display computer's choice
+    st.write(f"Computer chose: {computer_choice}")
+    st.code(items[computer])  # Display ASCII art for computer's choice
+
+    # Determine the winner
+    if user == computer:
+        st.write("It's a Tie!")
+    elif (user == 2 and computer == 1) or (user == 1 and computer == 0) or (user == 0 and computer == 2):
+        st.write("You win!")
+    else:
+        st.write("Computer wins!")
